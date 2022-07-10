c40bc6f7b85089730c3a516c8510a0b9f41af13f
Mark Ashton <mark.ashton@provi.com>
2022-07-09 20:45:14 -0500

    golfing day 1 part 2 a little

--- day1/part2.rb
+++ day1/part2.rb
@@ -2,14 +2,10 @@ def main
   input = File.read('input.txt')
   floor = 0
   input.chars.each.with_index do |c, i|
-    if c == '('
-      floor += 1
-    else
-      floor -= 1
-      if floor < 0
-        puts i + 1
-        break
-      end
+    c == '(' ? floor += 1 : floor -= 1
+    if floor < 0
+      puts i + 1
+      break
     end
   end
 end
